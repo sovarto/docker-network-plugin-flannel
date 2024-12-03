0d1df1d7f6d9b02e4665ad11da6caae2c641dc6c
Daniel Hilgarth <d.hilgarth@sovarto.com>
2024-12-03 18:52:49 +0100

    x

--- pkg/driver/network.go
+++ pkg/driver/network.go
@@ -129,11 +129,6 @@ func (d *flannelDriver) Join(request *network.JoinRequest) (*network.JoinRespons
 				RouteType:   types.NEXTHOP,
 				NextHop:     networkInfo.LocalGateway.String(),
 			},
-			{
-				Destination: "127.0.0.11/32",
-				RouteType:   types.NEXTHOP,
-				NextHop:     networkInfo.LocalGateway.String(),
-			},
 		},
 		DisableGatewayService: false,
 	}, nil
